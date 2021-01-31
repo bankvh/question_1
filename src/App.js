c9ed7d1ba8fc50b641525d5eeab88e6524ecbe8f
Bank <bankcrazynes@gmail.com>
2021-01-31 18:13:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const isPrime = num => {
+  for(let i = 2; i < num; i++)
+    if(num % i === 0) return false
+  return num > 1
 }
 
-export default App;
+function isSquare(n) {
+    return n > 0 && Math.sqrt(n) % 1 === 0;
+}
+
+function isFibonacci(numberToCheck){
+    return isSquare(5*numberToCheck*numberToCheck + 4) || isSquare(5*numberToCheck*numberToCheck - 4);
+}
+
+const calculatResult = (value, method) => {
+  switch(method){
+    case 'isPrime':
+      return isPrime(value)
+    case 'isFibonacci':
+      return isFibonacci(value)
+    default:
+      return false
+  }
+}
+
+function App() {
+
+  const [result, setResult] = useState(false)
+  const [value, setValue] = useState('')
+  const [selectMedthod, setSelectMethod] = useState('isPrime')
+
+  useEffect(()=>{
+    setResult(calculatResult(value, selectMedthod))
+  }, [value, selectMedthod])
+
+  const onInputChange = (e) => {
+    const {value} = e.target
+    let formatValue = value
+
+    if (value < 0){
+      formatValue = 1
+    } else if (value % 1 !== 0){
+      formatValue = Math.round(value)
+    }
+
+    setValue(formatValue)
+  }
+
+  const onSelectChange = (e) => {
+    setSelectMethod(e.target.value)
+  }
+
+  return (
+    <div className='container'>
+      <div className='column1'>
+        <input value={value} onChange={onInputChange} type='number' step='1'/>
+      </div>
+      <div className='column2'>
+        <select onChange={onSelectChange}>
+          <option value="isPrime">isPrime</option>
+          <option value="isFibonacci">isFibonacci</option>
+        </select>
+      </div>
+      <div className='column3'>
+        {String(result)}
+      </div>
+    </div>
+  )
+}
+
+export default App
